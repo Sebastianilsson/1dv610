e3046937cedb751e647dada928f0d9331bb95ece
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-09-11 20:32:29 +0200

    now showing day, date, month, year and time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,8 +5,27 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
-
+		$timeString = $this->getWeekday() . ", the " . $this->getDayOfMonth() . " of " . $this->getMonth() . " " . $this->getYear() . ", The time is " . $this->getTime();
 		return '<p>' . $timeString . '</p>';
+	}
+
+	public function getWeekday() {
+		return date('l');
+	}
+
+	public function getDayOfMonth() {
+		return date('jS');
+	}
+
+	public function getMonth() {
+		return date('F');
+	}
+
+	public function getYear() {
+		return date('Y');
+	}
+
+	public function getTime() {
+		return date('H:i:s');
 	}
 }
