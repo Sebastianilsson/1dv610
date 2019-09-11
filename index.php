41e12d8d9b2f0255326eb2c097ea45911c2beba7
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-09-11 21:35:13 +0200

    adding a tag for registration

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
+require_once('view/RegisterView.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
@@ -13,6 +14,7 @@ ini_set('display_errors', 'On');
 $v = new LoginView();
 $dtv = new DateTimeView();
 $lv = new LayoutView();
+$rv = new RegisterView();
 
 
 $lv->render(false, $v, $dtv);
