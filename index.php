821945f904d891f2212cb92760b62a5f3b7da991
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-09-12 16:23:40 +0200

    test

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 //CREATE OBJECTS OF THE VIEWS
-$v = new LoginView();
+$loginView = new LoginView();
 $dtv = new DateTimeView();
 $lv = new LayoutView();
 $rv = new RegisterView();
 
 
-$lv->render(false, $v, $dtv);
+$lv->render(false, $loginView, $dtv);
 
 if ($rv->isRegisterSet()) {
     echo $rv->renderRegistrationForm();
