b4a07f8fcb2da38192460aed5bd2bce842e48439
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2019-09-12 17:01:13 +0200

    moving logic to maincontroller

--- controller/MainController.php
+++ controller/MainController.php
@@ -3,6 +3,18 @@
 class MainController {
 
     public function __construct() {
-        
+        $this->loginView = new LoginView();
+        $this->dateTimeView = new DateTimeView();
+        $this->layoutView = new LayoutView();
+        $this->registerView = new RegisterView();
     }
+
+    public function router() {
+        if ($this->registerView->isRegisterSet()) {
+            echo $this->registerView->renderRegistrationForm();
+        } else {
+            $this->layoutView->render(false, $this->loginView, $this->dateTimeView);
+        }
+    }
+
 }
--- index.php
+++ index.php
@@ -5,21 +5,13 @@ require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
 require_once('view/LayoutView.php');
 require_once('view/RegisterView.php');
+require_once('controller/MainController.php');
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-//CREATE OBJECTS OF THE VIEWS
-$loginView = new LoginView();
-$dateTimeView = new DateTimeView();
-$layoutView = new LayoutView();
-$registerView = new RegisterView();
+// Create main controller
+$componentController = new MainController();
 
-
-$layoutView->render(false, $loginView, $dateTimeView);
-
-if ($registerView->isRegisterSet()) {
-    echo $registerView->renderRegistrationForm();
-}
-
+$componentController->router();
